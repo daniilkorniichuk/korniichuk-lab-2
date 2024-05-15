c77b67fd0f9a7281f1b0ea05cd2a5dcb0e2bd4a2
Daniil Korniichuk <daniil_korniichuk@gmail.com>
2024-05-15 15:29:34 +0300

    fixed one more time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://daniilkorniichuk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/korniichuck-lab-2/',
+  baseUrl: '/korniichuk-lab-2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
